726622c1462b7f1e421afe7d95d67549f3541679
Vladimir <example@gmail.com>
2020-07-24 08:33:27 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+var name = "Ivan";
+
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+let leftBorderWidth;
+
+//number
+//string - ""
+//true/false
+//null
+//undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'green',
+//     weight: 200
+// }
+//Symbol
+
+//alert(1234);
+//console.log(number);
+// let answer = confirm("Вам есть 18?")
+// console.log(answer);
+
+// let answer = prompt("Вам есть 18?");
+// console.log(answer);
+
+//console.log(4 + 'sdf')
+
+let isChecked = true,
+    isClose = false;
+
+//console.log(isChecked && isClose);
+
+//console.log(isChecked || isClose);
+
+// if (2*5 == 8*1) {
+//     console.log('Верно');
+// } else {
+//     console.log('Не верно');
+// }
+
+// let answer = confirm("Вам есть 18?");
+// if (answer) {
+//     console.log('Проходите')
+// } else {
+//     console.log('Сосите')
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Неправильно')
+// } else if (num > 100) {
+//     console.log('Много')
+// } else {
+//     console.log('Верно')
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i)
+// }
+
+function logging(a, b) {
+    console.log(a + b)
+}
+
+logging(3, 5);
+
+logging(6, 8);
